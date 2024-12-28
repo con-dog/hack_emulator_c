b8b19f19deaded6bccda6b384466da16cac3936b
Papillon <75226598+con-dog@users.noreply.github.com>
2024-12-29 12:39:47 +1300

    mux make main

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 #include "primitive_gates/and/and.h"
 #include "primitive_gates/or/or.h"
 #include "primitive_gates/xor/xor.h"
+#include "primitive_gates/mux/mux.h"
 
 int main(void)
 {
-
   And and = {
       .input.a = false,
       .input.b = false,
@@ -23,9 +23,16 @@ int main(void)
               .input.b = false,
           };
 
+  Mux mux = {
+      .input.a = false,
+      .input.b = true,
+      .input.sel = true,
+  };
+
   and_gate(&and);
   or_gate(& or);
   xor_gate(&xor);
+  mux_gate(&mux);
 
-  printf("%d\n%d\n%d\n", and.output.out, or.output.out, xor.output.out);
+  printf("%d\n%d\n%d\n%d\n", and.output.out, or.output.out, xor.output.out, mux.output.out);
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC=gcc
-CFLAGS=-Wall -Wextra -Iprimitive_gates/nand -Iprimitive_gates/not -Iprimitive_gates/and -Iprimitive_gates/or -Iprimitive_gates/xor
+CFLAGS=-Wall -Wextra -Iprimitive_gates/nand -Iprimitive_gates/not -Iprimitive_gates/and -Iprimitive_gates/or -Iprimitive_gates/xor -Iprimitive_gates/mux
 
-SRC=main.c primitive_gates/nand/nand.c primitive_gates/not/not.c primitive_gates/and/and.c primitive_gates/or/or.c primitive_gates/xor/xor.c
+SRC=main.c primitive_gates/nand/nand.c primitive_gates/not/not.c primitive_gates/and/and.c primitive_gates/or/or.c primitive_gates/xor/xor.c primitive_gates/mux/mux.c
 OBJ=$(SRC:.c=.o)
 
 main: $(OBJ)
