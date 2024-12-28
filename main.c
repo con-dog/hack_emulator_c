ae7eddcc925f7ba5466e8f63f991695dd29c973f
Papillon <75226598+con-dog@users.noreply.github.com>
2024-12-29 12:15:01 +1300

    ...

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include "primitive_gates/not/not.h"
 #include "primitive_gates/nand/nand.h"
+#include "primitive_gates/and/and.h"
+#include "primitive_gates/or/or.h"
 
 int main(void)
 {
-  Nand nand = {
+
+  And and = {
       .input.a = false,
       .input.b = false,
   };
 
-  nand_gate(&nand);
-  Not not = {
-      .input.in = nand.output.out};
-  not_gate(&not );
-  printf("%d\n%d\n", nand.output.out, not .output.out);
+  Or or = {
+            .input.a = false,
+            .input.b = false,
+        };
+
+  and_gate(&and);
+  or_gate(& or);
+  // nand_gate(&nand);
+  // Not not = {
+  //     .input.in = nand.output.out};
+  // not_gate(&not );
+  printf("%d\n%d\n", and.output.out, or.output.out);
 }
--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 CC=gcc
-CFLAGS=-Wall -Wextra -Iprimitive_gates/nand -Iprimitive_gates/not
+CFLAGS=-Wall -Wextra -Iprimitive_gates/nand -Iprimitive_gates/not -Iprimitive_gates/and -Iprimitive_gates/or
 
-SRC=main.c primitive_gates/nand/nand.c primitive_gates/not/not.c
+SRC=main.c primitive_gates/nand/nand.c primitive_gates/not/not.c primitive_gates/and/and.c primitive_gates/or/or.c
 OBJ=$(SRC:.c=.o)
 
 main: $(OBJ)
