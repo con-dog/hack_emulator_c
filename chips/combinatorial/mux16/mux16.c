0bd72fb86003976ae6d4faaaf336417549a6cc08
Papillon <75226598+con-dog@users.noreply.github.com>
2024-12-29 16:14:07 +1300

    ...

--- chips/combinatorial/mux16/mux16.c
+++ chips/combinatorial/mux16/mux16.c
@@ -10,7 +10,7 @@ void mux16_chip(Mux16 *mux16)
     mux_chips[i].input.a = mux16->input.a[i];
     mux_chips[i].input.b = mux16->input.b[i];
     mux_chips[i].input.sel = mux16->input.sel;
-    mux_gate(&mux_chips[i]);
+    mux_chip(&mux_chips[i]);
     mux16->output.out[i] = mux_chips[i].output.out;
   }
 }
