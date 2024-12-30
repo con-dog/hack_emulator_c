5ddd50bf2f0cefa075f61df709c93088315c02ed
Papillon <75226598+con-dog@users.noreply.github.com>
2024-12-30 18:30:07 +1300

    ...

--- components/combinatorial/logic-gates/multi-bit/and16/and16.c
+++ components/combinatorial/logic-gates/multi-bit/and16/and16.c
@@ -4,12 +4,12 @@
 
 void and16_gate(And16 *and16_unit)
 {
-  And and_gate_units[WORD_SIZE];
+  And and_units[WORD_SIZE];
   for (int i = 0; i < WORD_SIZE; i++)
   {
-    and_gate_units[i].input.a = and16_unit->input.a[i];
-    and_gate_units[i].input.b = and16_unit->input.b[i];
-    and_gate(&and_gate_units[i]);
-    and16_unit->output.out[i] = and_gate_units[i].output.out;
+    and_units[i].input.a = and16_unit->input.a[i];
+    and_units[i].input.b = and16_unit->input.b[i];
+    and_gate(&and_units[i]);
+    and16_unit->output.out[i] = and_units[i].output.out;
   }
 }
