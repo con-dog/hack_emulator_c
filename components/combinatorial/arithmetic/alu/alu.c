c6871bdc65149aba4f713c4ea6a7f49474e1df2c
Papillon <75226598+con-dog@users.noreply.github.com>
2024-12-31 12:46:51 +1300

    pretty sure alu works

--- components/combinatorial/arithmetic/alu/alu.c
+++ components/combinatorial/arithmetic/alu/alu.c
@@ -1,4 +1,13 @@
+#include <string.h>
 #include "alu.h"
+#include "mux16.h"
+#include "not16.h"
+#include "and16.h"
+#include "add16.h"
+#include "or-8way.h"
+#include "or.h"
+#include "not.h"
+#include "types.h"
 
 /**
  * ALU (Arithmetic Logic Unit):
@@ -25,5 +34,112 @@
 // if (no == 1) sets out = !out   // bitwise not
 void alu_chip(Alu *alu)
 {
-  // TODO
+  /*
+   * INPUTS
+   * X PRESETS
+   */
+  // ZERO X
+  Mux16 mux16_block_x = {
+      .input.b = {[0 ... WORD_SIZE - 1] = 0},
+      .input.sel = alu->input.zx,
+  };
+  memcpy(mux16_block_x.input.a, alu->input.x, sizeof(mux16_block_x.input.a));
+  mux16_chip(&mux16_block_x);
+
+  // NOT X
+  Not16 not16_block_x;
+  memcpy(not16_block_x.input.in, mux16_block_x.output.out, sizeof(not16_block_x.input.in));
+  not16_gate(&not16_block_x);
+
+  // X BLOCK OUT
+  Mux16 mux16_block_x_out = {
+      .input.sel = alu->input.nx};
+  memcpy(mux16_block_x_out.input.a, mux16_block_x.output.out, sizeof(mux16_block_x_out.input.a));
+  memcpy(mux16_block_x_out.input.b, not16_block_x.output.out, sizeof(mux16_block_x_out.input.b));
+  mux16_chip(&mux16_block_x_out);
+
+  /*
+   * Y PRESETS
+   */
+  // ZERO Y
+  Mux16 mux16_block_y = {
+      .input.b = {[0 ... WORD_SIZE - 1] = 0},
+      .input.sel = alu->input.zy,
+  };
+  memcpy(mux16_block_y.input.a, alu->input.y, sizeof(mux16_block_y.input.a));
+  mux16_chip(&mux16_block_y);
+
+  // NOT Y
+  Not16 not16_block_y;
+  memcpy(not16_block_y.input.in, mux16_block_y.output.out, sizeof(not16_block_y.input.in));
+  not16_gate(&not16_block_y);
+
+  // Y BLOCK OUT
+  Mux16 mux16_block_y_out = {
+      .input.sel = alu->input.ny};
+  memcpy(mux16_block_y_out.input.a, mux16_block_y.output.out, sizeof(mux16_block_y_out.input.a));
+  memcpy(mux16_block_y_out.input.b, not16_block_y.output.out, sizeof(mux16_block_y_out.input.b));
+  mux16_chip(&mux16_block_y_out);
+
+  /*
+   * HANDLE F
+   */
+  And16 and16_block_f;
+  memcpy(and16_block_f.input.a, mux16_block_x_out.output.out, sizeof(and16_block_f.input.a));
+  memcpy(and16_block_f.input.b, mux16_block_y_out.output.out, sizeof(and16_block_f.input.b));
+  and16_gate(&and16_block_f);
+
+  Add16 add16_block_f;
+  memcpy(add16_block_f.input.a, mux16_block_x_out.output.out, sizeof(add16_block_f.input.a));
+  memcpy(add16_block_f.input.b, mux16_block_y_out.output.out, sizeof(add16_block_f.input.b));
+  add16_chip(&add16_block_f);
+
+  // CHOOSE function
+  Mux16 mux16_block_f_out = {
+      .input.sel = alu->input.f,
+  };
+  memcpy(mux16_block_f_out.input.a, and16_block_f.output.out, sizeof(mux16_block_f_out.input.a));
+  memcpy(mux16_block_f_out.input.b, add16_block_f.output.out, sizeof(mux16_block_f_out.input.b));
+  mux16_chip(&mux16_block_f_out);
+
+  /*
+   * OUTPUTS
+   * NO
+   */
+  Not16 not16_block_no;
+  memcpy(not16_block_no.input.in, mux16_block_f_out.output.out, sizeof(not16_block_no.input.in));
+  not16_gate(&not16_block_no);
+
+  Mux16 mux16_block_no_out = {
+      .input.sel = alu->input.no,
+  };
+  memcpy(mux16_block_no_out.input.a, mux16_block_f_out.output.out, sizeof(mux16_block_no_out.input.a));
+  memcpy(mux16_block_no_out.input.b, not16_block_no.output.out, sizeof(mux16_block_no_out.input.b));
+  mux16_chip(&mux16_block_no_out);
+
+  // ALU OUT
+  memcpy(alu->output.out, mux16_block_no_out.output.out, sizeof(alu->output.out));
+  alu->output.ng = alu->output.out[15];
+
+  /*
+   * ZR
+   */
+  Or_8way or_8way_block_1, or_8way_block_2;
+  memcpy(or_8way_block_1.input.in, &alu->output.out[0], sizeof(or_8way_block_1.input.in));
+  memcpy(or_8way_block_2.input.in, &alu->output.out[8], sizeof(or_8way_block_2.input.in));
+  or_8way_gate(&or_8way_block_1);
+  or_8way_gate(&or_8way_block_2);
+
+  Or or_unit = {
+      .input.a = or_8way_block_1.output.out,
+      .input.b = or_8way_block_2.output.out,
+  };
+  or_gate(&or_unit);
+
+  Not not_unit = {
+      .input.in = or_unit.output.out,
+  };
+  not_gate(&not_unit);
+
+  alu->output.zr = not_unit.output.out;
 }
--- main.c
+++ main.c
@@ -3,29 +3,30 @@
 #include "components/combinatorial/arithmetic/add16/add16.h"
 #include "components/combinatorial/arithmetic/inc16/inc16.h"
 #include "components/combinatorial/arithmetic/full-adder/full-adder.h"
+#include "components/combinatorial/arithmetic/alu/alu.h"
 
 int main(void)
 {
-  Inc16 inc16_unit = {
-      .input.in = {
-          [1] = 1, [2] = 2},
+  Alu alu = {
+      .input.x = {0},
+      .input.y = {[0 ... WORD_SIZE - 1] = 1},
+      //
+      .input.zx = 1,
+      .input.nx = 1,
+      //
+      .input.zy = 1,
+      .input.ny = 0,
+      //
+      .input.f = 1,
+      .input.no = 0,
   };
-  inc16_chip(&inc16_unit);
+  alu_chip(&alu);
 
   for (int i = WORD_SIZE - 1; i >= 0; i--)
   {
-    printf("%d", inc16_unit.output.out[i]);
+    printf("%d", alu.output.out[i]);
   }
-  // Add16 add16_unit = {
-  //     .input.a = {[0 ... WORD_SIZE - 1] = 1},
-  //     .input.b = {[0 ... WORD_SIZE - 1] = 1},
-  // };
-  // add16_chip(&add16_unit);
-
-  // for (int i = WORD_SIZE - 1; i >= 0; i--)
-  // {
-  //   printf("%d", add16_unit.output.out[i]);
-  // }
-
   printf("\n");
+  printf("%d\n", alu.output.zr);
+  printf("%d\n", alu.output.ng);
 }
