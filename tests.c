08f69541a2f1c2a114a56baeb6c7308983aea508
Papillon <75226598+con-dog@users.noreply.github.com>
2025-01-01 12:41:21 +1300

    ...

--- tests.c
+++ tests.c
@@ -10,6 +10,14 @@ int main(void)
       .tests_run = 0,
   };
 
+  /*
+   * START: ADD ALL TESTS IN HERE
+   */
   test_nand(&counter);
+  // NEXT TEST GOES HERE
+  /*
+   * END: ADD ALL TESTS IN HERE
+   */
+
   printf("\nTests run: \t%d\nTests failing: \t%d\n", counter.tests_run, counter.tests_failed);
 }
