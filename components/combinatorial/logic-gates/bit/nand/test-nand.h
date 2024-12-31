e0443fed90fcb1169260373f66329b77ef2b0ed4
Papillon <75226598+con-dog@users.noreply.github.com>
2025-01-01 12:39:06 +1300

    fix tests

--- components/combinatorial/logic-gates/bit/nand/test-nand.h
+++ components/combinatorial/logic-gates/bit/nand/test-nand.h
@@ -1,15 +1,7 @@
 #ifndef TEST_NAND_H
 #define TEST_NAND_H
 
-#include <stdbool.h>
 #include "test-utils.h"
-
-typedef struct Nand_TT
-{
-  bool a;
-  bool b;
-  bool c;
-} Nand_TT;
 
 void test_nand(Test_Counter *counter);
 
